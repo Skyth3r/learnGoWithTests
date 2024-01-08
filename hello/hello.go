10a11cf787a242cdb0d40ba66edc585315449066
Akash Goswami <Skyth3r@users.noreply.github.com>
2024-01-08 21:26:39 +0000

    ✨ Added hello directory

---
+++ hello/hello.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingsPrefix(language) + name
+}
+
+func greetingsPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
+
+func main() {
+	fmt.Println(Hello("world", ""))
+}
